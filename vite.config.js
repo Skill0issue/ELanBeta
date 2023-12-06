d76d2fe4881736229de722c91fdaae1eb9582022
Himanshu Sekhar <hsmaharaj1@gmail.com>
2023-12-06 22:04:40 +0530

    Deployment Config changes

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import tailwindcss from 'tailwindcss';
+import VitePages from 'vite-plugin-pages';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-})
+  base: '/',
+  plugins: [react(), tailwindcss(), VitePages()],
+});
