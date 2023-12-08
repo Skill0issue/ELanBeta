9e13cf180b935c94c9bdb900e19847fa4e3fdda9
Himanshu Sekhar <hsmaharaj1@gmail.com>
2023-12-09 01:26:35 +0530

    Social cause image

--- src/components/SocialCause.jsx
+++ src/components/SocialCause.jsx
@@ -2,6 +2,7 @@ import Navbar from "./Navbar";
 import Footer from "./Footer";
 import { AiOutlineArrowLeft } from "react-icons/ai";
 import { Link } from "react-router-dom";
+import dog from "/src/assets/socialCause/feedDog.jpg"
 
 const SocialCause = () => {
   return (
@@ -19,7 +20,7 @@ const SocialCause = () => {
           <h1 className='font-Moul text-[#850E35] text-4xl md:text-5xl lg:text-6xl p-8 mb-6 md:ml-4 overflow-hidden'>
             IKSHANA-LOVE ANIMALS
           </h1>
-          <img src="/src/assets/socialCause/feedDog.jpg" alt="IKSHANA IMG" className='w-[40vw] h-auto aspect-video object-cover rounded-xl mx-auto' />
+          <img src={dog} alt="IKSHANA IMG" className='w-[40vw] h-auto aspect-video object-cover rounded-xl mx-auto' />
           <div className="p-4 mx-4 mt-8 mb-8 overflow-hidden text-lg leading-snug md:text-2xl lg:text-3xl xl:text-4xl 2xl:text-5xl font-MoulPali md:mx-8 lg:mx-12 xl:mx-16 2xl:mx-20 md:px-8 lg:px-12 xl:px-16 2xl:px-20">
             Perusing articles on environmental issues, it becomes evident that animals often suffer due to human activities, despite their rightful place on Earth. This shared planet gives every species an inherent right to its resources. Acknowledging this, “Elan & ηVision” introduces the initiative "IKSHANA - LOVE ANIMALS" this year.
 
