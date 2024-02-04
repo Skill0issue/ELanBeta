e7e94470c9639e607628208586e7e336209fb027
Nihar Kartikeya <127834591+Skill0issue@users.noreply.github.com>
2024-02-05 00:19:28 +0530

    added the workshop card and made some changes

--- src/components/WorkshopCard.jsx
+++ src/components/WorkshopCard.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { Card, CardHeader, CardBody, CardFooter, Typography, Button } from "@material-tailwind/react";
 
-const WorkshopCard = ({ title, description, img, registration, ...props }) => {
+const WorkshopCard = ({ title, description, img, registration,timefrom,timeto,mode, ...props }) => {
     return (
-        <Card className="mt-6 w-full md:w-96 rounded p-4" style={{ backgroundColor: "#F68989" }}>
+        <Card className="w-full p-4 mt-6 rounded- md:w-96" style={{ backgroundColor: "#F68989" }}>
             <CardHeader color="blue-gray" className="relative h-56">
                 <img
                     src={img}
@@ -11,16 +11,23 @@ const WorkshopCard = ({ title, description, img, registration, ...props }) => {
                 />
             </CardHeader>
             <CardBody>
-                <Typography variant="h5" color="blue-gray" className="mb-2">
+                <Typography variant="h5" color="blue-gray" className="mb-2 font-bold">
                     {title}
                 </Typography>
-                <Typography>
+                <Typography className="pb-4 font-[500] text-justify overflow-clip h-36">
                     {description}
+                </Typography>
+                <Typography className="py-2 font-semibold">
+                    Mode: {mode}
+                    <br />
+                    From: {timefrom}
+                    <br />  
+                    To: {timeto}
                 </Typography>
             </CardBody>
             <CardFooter className="pt-0">
-                <a href={registration}>
-                    <Button style={{ backgroundColor: "#874356" }} ripple="light" className="rounded-full text-lg md:text-base px-4 md:px-3 py-2 md:py-1">REGISTER</Button>
+                <a href={registration} target="blank">
+                    <Button style={{ backgroundColor: "#874356" }} ripple="light" className="px-4 py-2 text-lg rounded-full md:text-base md:px-3 md:py-1">REGISTER</Button>
                 </a>
             </CardFooter>
         </Card>
--- src/components/Workshops.jsx
+++ src/components/Workshops.jsx
@@ -1,8 +1,9 @@
-import Navbar from './Navbar';
-import Footer from './Footer';
-import { AiOutlineArrowLeft } from 'react-icons/ai';
-import { Link } from 'react-router-dom';
-import WorkshopCard from './WorkshopCard';
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import { AiOutlineArrowLeft } from "react-icons/ai";
+import { Link } from "react-router-dom";
+import WorkshopCard from "./WorkshopCard";
+import MMWS from '../assets/workshops/poster copy-2.png'
 
 const Workshops = () => {
   return (
@@ -14,15 +15,25 @@ const Workshops = () => {
             <AiOutlineArrowLeft
               size={60}
               color={'#874356'}
-              className="w-6 h-6 md:w-14 md:h-12 ml-2 sm:ml-1 md:ml-8 lg:ml-4"
+              className="w-6 h-6 ml-2 md:w-14 md:h-12 sm:ml-1 md:ml-8 lg:ml-4"
             />
           </Link> */}
-          <h1 className='font-Moul text-[#850E35] text-4xl md:text-5xl lg:text-6xl p-6 mb-6 md:ml-4 overflow-hidden'>
+          <h1 className="font-Moul text-[#850E35] text-4xl md:text-5xl lg:text-6xl p-6 mb-6 md:ml-4 overflow-hidden">
             About Our Workshops
           </h1>
-          <div className="sm:p-2 md:p-8 text-base font-bold leading-snug sm:text-lg md:text-xl lg:text-2xl xl:text-2xl font-Prompt sm:mx-4 md:mx-6 lg:mx-10 xl:mx-16" style={{ marginLeft: '2rem', marginRight: '2rem', '@media (max-width: 450px)': { margin: '1rem' } }}>
-            Workshops are an essential part of our fest. They are intended for hands-on experiential learning. Our workshops are designed and planned in such a way that they improve students' critical thinking skills. Some of our key workshops include
-            <ul className='text-[#874356] lg:text-xl mg:text-lg sm:text-base'>
+          <div
+            className="text-base font-bold leading-snug sm:p-2 md:p-8 sm:text-lg md:text-xl lg:text-2xl xl:text-2xl font-Prompt sm:mx-4 md:mx-6 lg:mx-10 xl:mx-16"
+            style={{
+              marginLeft: "2rem",
+              marginRight: "2rem",
+              "@media (max-width: 450px)": { margin: "1rem" },
+            }}
+          >
+            Workshops are an essential part of our fest. They are intended for
+            hands-on experiential learning. Our workshops are designed and
+            planned in such a way that they improve students&apos; critical thinking
+            skills. Some of our key workshops include
+            <ul className="text-[#874356] lg:text-xl mg:text-lg sm:text-base">
               <li>Ethical Hacking and Cyber Security</li>
               <li>Facial and Emotional Recognition with Deep Learning</li>
               <li>Artificial Intelligence and Machine Learning</li>
@@ -31,20 +42,27 @@ const Workshops = () => {
             </ul>
           </div>
           <div className="h-auto text-3xl p-4 mb-6 overflow-hidden text-[#850E35] font-bold flex flex-col items-center">
-            <span className="inline-block overflow-hidden text-center">NO UPCOMING WORKSHOPS</span>
-            <span className="inline-block overflow-hidden text-center">Stay tuned for further updates :)</span>
-            {/* <WorkshopCard
-              title={"Sample Event"}
-              description={"Just Trying out this sample Just Trying out this sample "}
-              img={""}
-              registration={""}
-            /> */}
+            {/* <span className="inline-block overflow-hidden text-center">NO UPCOMING WORKSHOPS</span>
+            <span className="inline-block overflow-hidden text-center">Stay tuned for further updates :)</span> */}
+            <WorkshopCard
+              title={"Market Mastery Workshop Series"}
+              description={
+                "This workshop covers all aspects essential for a strong foundation in stock trading, making it ideal for beginners.  Learn from experienced professionals in the field who will share practical insights and valuable tips. Whether you aspire to build long-term wealth or generate additional income, this workshop empowers you to take control of your financial future through informed stock trading. "
+              }
+              img={MMWS}
+              timefrom={"24th Feb"}
+              timeto={"25th Feb"}
+              mode = {"online"}
+              registration={
+                "https://unstop.com/workshops-webinars/market-mastery-workshop-series-elan-ivision-2024-iit-hyderabad-882942"
+              }
+            />
           </div>
         </section>
         <Footer />
       </div>
     </>
   );
-}
+};
 
 export default Workshops;
